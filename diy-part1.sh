4917fbbec2ae6d41b80dbaa3fb339d7e35f340ec
kai.zhang1 <kai.zhang1@nio.com>
2021-09-11 15:34:24 +0800

    add files

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,30 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+packages=(
+    https://github.com/toss-a/luci-theme-atmaterial
+    https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
+    https://github.com/aboutboy/luci-theme-butongwifi
+    https://github.com/tty228/luci-app-serverchan
+    https://github.com/rufengsuixing/luci-app-adguardhome
+    https://github.com/jefferymvp/luci-app-koolproxyR
+    https://github.com/project-lede/luci-app-godproxy
+    https://github.com/destan19/OpenAppFilter
+    https://github.com/zzsj0928/luci-app-pushbot
+    https://github.com/jerrykuku/luci-app-ttnode
+    https://github.com/lisaac/luci-app-dockerman
+    https://github.com/lisaac/luci-app-diskman
+    https://github.com/jerrykuku/lua-maxminddb
+    https://github.com/jerrykuku/luci-app-vssr
+    https://github.com/esirplayground/luci-app-poweroff
+    https://github.com/rufengsuixing/luci-app-onliner
+)
+mkdir -p package/3rdparty
+for package in ${packages[@]}
+do
+    echo "syncing $package"
+    name=$(echo $package | awk -F '/' '{print $NF}')
+    git clone $package package/3rdparty/$name
+done
