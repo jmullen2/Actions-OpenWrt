db2a9da6ef19fb4b954f27d02f96ed92dec32dac
apple <apple@appledeMacBook.local>
2022-03-01 21:32:13 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,22 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # Add a feed source
 packages=(
-    https://github.com/toss-a/luci-theme-atmaterial
-    https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
-    https://github.com/aboutboy/luci-theme-butongwifi
-    https://github.com/tty228/luci-app-serverchan
-    https://github.com/rufengsuixing/luci-app-adguardhome
-    https://github.com/jefferymvp/luci-app-koolproxyR
-    https://github.com/project-lede/luci-app-godproxy
-    https://github.com/destan19/OpenAppFilter
-    https://github.com/zzsj0928/luci-app-pushbot
-    https://github.com/jerrykuku/luci-app-ttnode
-    https://github.com/lisaac/luci-app-dockerman
-    https://github.com/lisaac/luci-app-diskman
-    https://github.com/jerrykuku/lua-maxminddb
-    https://github.com/jerrykuku/luci-app-vssr
     https://github.com/esirplayground/luci-app-poweroff
     https://github.com/rufengsuixing/luci-app-onliner
 )
