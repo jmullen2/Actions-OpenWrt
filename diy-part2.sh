5f191a0de74b99040e3be18bcd66133905dcaf1f
apple <apple@appledeMacBook.local>
2022-03-03 09:04:22 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/Online User/在线用户/g' package/3rdparty/luci-app-onliner/luasrc/controller/onliner.lua
